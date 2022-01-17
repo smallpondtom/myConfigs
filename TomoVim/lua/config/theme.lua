b83d3b43b905dbf45bed8a28c57c5b4cd0141cd6
smallpondtom <usasmpdtom@gmail.com>
2022-01-16 22:16:53 -0500

    Allow different themes for wsl and linux.

--- TomoVim/lua/config/theme.lua
+++ TomoVim/lua/config/theme.lua
@@ -9,7 +9,31 @@ require('monokai').setup {}
 require('monokai').setup { palette = require('monokai').pro }
 require('monokai').setup { palette = require('monokai').soda }
 
-vim.cmd[[colorscheme monokai]]
+-- Detect whether it is WSL or pure linux
+local is_wsl = vim.api.nvim_exec([[
+  function! Is_WSL() abort
+    let proc_version = '/proc/version'
+    return filereadable(proc_version)
+          \  ? !empty(filter(
+          \    readfile(proc_version, '', 1), { _, val -> val =~? 'microsoft' }))
+          \  : v:false
+  endfunction
+
+  echo Is_WSL()
+]], true)
+
+if os then -- when Linux
+  if is_wsl=="1" then  -- if WSL
+    vim.cmd[[colorscheme monokai]]
+    vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+  elseif is_wsl=="0" then -- if pure Linux
+    vim.cmd[[colorscheme gruvbox]]
+  else
+    print("Not valid -> requires debugging.")
+  end
+else -- if windows or Mac
+  print("I hate MacOS. Use Linux.")
+end
 
 --[[ require('lualine').setup {
   options = {
@@ -19,6 +43,5 @@ vim.cmd[[colorscheme monokai]]
   }
 } ]]
 
-vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 vim.cmd("hi clear LineNr")
 vim.cmd("hi clear SignColumn")
